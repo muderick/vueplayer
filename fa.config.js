71f915f408816ef1b51153289314c5606861de70
muderick <mumiaderick@gmail.com>
2023-03-02 10:21:36 +0300

    modified

---
+++ fa.config.js
@@ -0,0 +1,12 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+
+import { faShuffle, faRepeat, faVolumeXmark, faVolumeLow, faVolumeHigh } from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  faShuffle,
+  faRepeat,
+  faVolumeXmark,
+  faVolumeLow,
+  faVolumeHigh
+);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from "vue";
+import { createApp} from "vue";
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/index.css";
 
-createApp(App).use(router).mount("#app");
+import "../fa.config"
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount("#app");
